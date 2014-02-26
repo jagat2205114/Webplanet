006e6b3ffa4e2f0ccd8d8f43a0520d71fa6038c9
Jagat Prakash Chandola <jagat@webplanet.co.in>
2014-02-26 17:50:48 +0530

    Change

--- jagat/jagat.php
+++ jagat/jagat.php
@@ -1,6 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Calcutta");
 if($_SERVER['HTTP_HOST']== 'localhost'){
-    
+    define('HTTP_SERVER', 'http://localhost/dailyzing/admin/');
+define('HTTP_CATALOG', 'http://localhost/dailyzing/');
 }
 ?>
